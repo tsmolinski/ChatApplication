b9c206a522dbcf33459f4e04bb421529f8d6c6df
tsmolinski <tsmolinski3@gmail.com>
2025-07-13 18:14:59 +0200

    Added rest of ClientManager functionalities (from last commit), added UI layour for Client.

--- Client/ClientManager.cpp
+++ Client/ClientManager.cpp
@@ -1,10 +1,31 @@
 #include "ClientManager.h"
-
-ClientManager::ClientManager(QObject *parent)
-    : QObject{parent}
-{}
+#include <QTcpSocket>
 
 ClientManager::ClientManager(QHostAddress ip, ushort port, QObject *parent)
+    : QObject{parent}
+    , ipAddress(ip)
+    , port(port)
 {
+    tcpSocket = new QTcpSocket(this);
 
+    connect(tcpSocket, &QTcpSocket::connected, this, &ClientManager::connected);
+    connect(tcpSocket, &QTcpSocket::disconnected, this, &ClientManager::disconnected);
+    connect(tcpSocket, &QTcpSocket::readyRead, this, &ClientManager::readyRead);
+}
+
+void ClientManager::connectToServer()
+{
+    tcpSocket->connectToHost(ipAddress, port);
+}
+
+void ClientManager::sendMessage(QString message)
+{
+    tcpSocket->write(message.toUtf8());
+}
+
+void ClientManager::readyRead()
+{
+    QByteArray data = tcpSocket->readAll();
+
+    emit dataReceived(data);
 }
--- Client/ClientManager.h
+++ Client/ClientManager.h
@@ -2,17 +2,32 @@
 #define CLIENTMANAGER_H
 
 #include <QObject>
+#include <QHostAddress>
 
 class QTcpSocket;
-//class QHostAddress;
 
 class ClientManager : public QObject
 {
     Q_OBJECT
 public:
-    //explicit ClientManager(QHostAddress ip = QHostAddress::LocalHost, QObject *parent = nullptr);
+    explicit ClientManager(QHostAddress ip = QHostAddress::LocalHost, ushort port = 4500, QObject *parent = nullptr);
+
+    void connectToServer();
+    void sendMessage(QString message);
 
 signals:
+    void connected();
+    void disconnected();
+    void dataReceived(QByteArray data);
+
+private slots:
+    void readyRead();
+
+private:
+    QTcpSocket* tcpSocket;
+    QHostAddress ipAddress;
+    ushort port;
+
 };
 
 #endif // CLIENTMANAGER_H
